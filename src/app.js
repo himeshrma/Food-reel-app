c61c3b1a2567a7e5603254146233befe08eb114e
himeshrma <sharmahimesh2003@gmail.com>
2025-10-24 03:56:54 +0530

    Initia Commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 //App.js me creates Server and connects to Database
 
 const express = require("express");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
 
 app.use(express.json()); // Middleware to parse JSON request bodies
-
-const cookieParser = require("cookie-parser");
-
 app.use(cookieParser()); // Middleware to parse cookies
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 
-router.post("/user/register", (req, res) => {});
+router.post("/user/register", authController.registerUser);
+
 module.exports = router;
